e34ff28aa9332c0deb4541ccabae6c146a43f8d1
Alexei1R <rusu.alexei.2002@gmail.com>
2023-12-18 18:09:44 +0200

    Test Viewport Imgui

--- app/FastMeasure/Algorithm/HoughTransform.cpp
+++ app/FastMeasure/Algorithm/HoughTransform.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "HoughTransform.h"
+
+
+
+namespace Atom {
+    HoughTransorm::HoughTransorm(Atom::LidarReadLayer& lidarReadLayer)
+            : Layer("HoughTransorm") , m_LidarReadLayer(lidarReadLayer)
+    {
+
+
+
+
+    }
+
+    HoughTransorm::~HoughTransorm() {
+
+    }
+
+    void HoughTransorm::OnAttach() {}
+
+    void HoughTransorm::OnDetach() {}
+
+
+
+    void HoughTransorm::OnUpdate() {
+
+
+    }
+
+
+
+
+    void HoughTransorm::OnFixedUpdate() {
+    }
+}
--- app/FastMeasure/Algorithm/HoughTransform.h
+++ app/FastMeasure/Algorithm/HoughTransform.h
@@ -4,11 +4,31 @@
 
 #ifndef FASTMEASURE_HOUGHTRANSFORM_H
 #define FASTMEASURE_HOUGHTRANSFORM_H
+#include "FastMeasure/EmbededPlatform/LidarReadLayer.h"
+#include "FastMeasure/fastmeasurepch.h"
 
 
-class HoughTransform {
 
-};
+namespace Atom {
+    class HoughTransorm : public Layer {
+    public:
+        HoughTransorm(Atom::LidarReadLayer& lidarReadLayer);
+        ~HoughTransorm();
+
+        virtual void OnAttach() override;
+        virtual void OnDetach() override;
+        virtual void OnUpdate() override;
+        virtual void OnFixedUpdate() override;
+
+    private:
+    LidarReadLayer& m_LidarReadLayer;
+
+
+
+    };
+
+
+}
 
 
 #endif //FASTMEASURE_HOUGHTRANSFORM_H
